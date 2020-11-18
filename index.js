41da4d1e554ddc2075add396077b6d88af4fae74
Hamid Asraf <hamid.asraf@techgenies.com>
2020-11-19 04:20:34 +0530

    added allow origin code

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(helmet());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,authorization,x-access-token, Content-Encoding');
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT,POST,DELETE,GET');
+      return res.status(200).json({});
+    }
+    next();
+});
+
+
 app.use("/api/", publicAPI);
 
 app.get('/', function(req, res){
